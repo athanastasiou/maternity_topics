a97936230ad40b86411295021d5dd7408b3a0707
Athanasios Anastasiou <athanasios.anastasiou@nihr.ac.uk>
2025-03-27 11:47:03 +0000

    Added a DROP in the ingest_embeddings to be able to execute the query repeatedly

--- data/import_embeddings.sql
+++ data/import_embeddings.sql
@@ -1,3 +1,4 @@
+DROP TABLE IF EXISTS DetailsSentences;
 CREATE TABLE DetailsSentences AS (SELECT * FROM 'OUT_InfrasThemeDetails_EXPORTED_data_embeddings.csv' LIMIT 1);
 ALTER TABLE DetailsSentences DROP COLUMN embedding;
 ALTER TABLE DetailsSentences ADD COLUMN embedding DOUBLE[384];
